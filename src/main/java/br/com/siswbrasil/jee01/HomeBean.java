fe1ef99a05ec6dec0b1e7cf782b8cfbd14de8a44
sistemaswebbrasil <adriano.faria@outlook.com.br>
2020-04-08 20:55:02 -0300

    Adicionado o plugin Lombok

--- src/main/java/br/com/siswbrasil/jee01/HomeBean.java
+++ src/main/java/br/com/siswbrasil/jee01/HomeBean.java
@@ -3,18 +3,15 @@ package br.com.siswbrasil.jee01;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Named
 @RequestScoped
 public class HomeBean {
 
 	private String initialMessage = "Hello World!";
-
-	public String getInitialMessage() {
-		return initialMessage;
-	}
-
-	public void setInitialMessage(String initialMessage) {
-		this.initialMessage = initialMessage;
-	}
 
 }
