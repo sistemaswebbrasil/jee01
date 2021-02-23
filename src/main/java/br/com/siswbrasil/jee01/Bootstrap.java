9bc77a3812752462060be8f409f95cd5ea08f446
Adriano Faria Alves <adriano.faria@outlook.com.br>
2021-02-23 10:35:58 -0300

    Testes

--- src/main/java/br/com/siswbrasil/jee01/Bootstrap.java
+++ src/main/java/br/com/siswbrasil/jee01/Bootstrap.java
@@ -1,5 +1,6 @@
 package br.com.siswbrasil.jee01;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -12,10 +13,12 @@ import javax.inject.Inject;
 
 import br.com.siswbrasil.jee01.exception.BusinessException;
 import br.com.siswbrasil.jee01.model.Organization;
+import br.com.siswbrasil.jee01.model.Product;
 import br.com.siswbrasil.jee01.model.Role;
 import br.com.siswbrasil.jee01.model.Task;
 import br.com.siswbrasil.jee01.model.User;
 import br.com.siswbrasil.jee01.service.OrganizationService;
+import br.com.siswbrasil.jee01.service.ProductService;
 import br.com.siswbrasil.jee01.service.RoleService;
 import br.com.siswbrasil.jee01.service.TaskService;
 import br.com.siswbrasil.jee01.service.UserService;
@@ -42,6 +45,9 @@ public class Bootstrap {
 	
 	@Inject
 	private RoleService roleService;
+	
+	@Inject
+	private ProductService productService;
 
 	@PostConstruct
 	public void init() {
@@ -58,6 +64,7 @@ public class Bootstrap {
 				initOrganizations();
 				initRoles();
 				initialUsers();
+				initialProducts();
 			} catch (BusinessException e) {
 				LOG.log(Level.SEVERE,
 						"Falha ao gerar os dados iniciais de desenvolvimento , aparentemente erro nos dados enviados para o servidor",
@@ -66,6 +73,53 @@ public class Bootstrap {
 				LOG.log(Level.SEVERE, "Falha ao gerar os dados iniciais de desenvolvimento ", e);
 			}
 		}
+	}
+	
+	private void initialProducts() throws Throwable {
+		Product product1 = new Product(null,"Arroz",new BigDecimal(25.50));
+		Product product2 = new Product(null,"Feijão",new BigDecimal(9.50));
+		Product product3 = new Product(null,"Macarrão",new BigDecimal(5.50));
+		Product product4 = new Product(null,"Óleo",new BigDecimal(9.0));
+		Product product5 = new Product(null,"Azeite",new BigDecimal(22.99));
+		Product product6 = new Product(null,"Linguiça",new BigDecimal(15.20));
+		Product product7 = new Product(null,"Biscoito",new BigDecimal(3.50));
+		Product product8 = new Product(null,"Pão de Forma",new BigDecimal(6.20));
+		Product product9 = new Product(null,"Cerveja Lata",new BigDecimal(3.50));
+		Product product10 = new Product(null,"Água 500ml",new BigDecimal(1.50));
+		
+		Product product11 = new Product(null,"Massa de Tomate",new BigDecimal(5.50));
+		Product product12 = new Product(null,"Saco de Lixo",new BigDecimal(12.0));
+		Product product13 = new Product(null,"Café",new BigDecimal(14.00));
+		Product product14 = new Product(null,"Sabonete",new BigDecimal(2.50));
+		Product product15 = new Product(null,"Escova de dente",new BigDecimal(15.99));
+		Product product16 = new Product(null,"Pasta de dente",new BigDecimal(3.00));
+		Product product17 = new Product(null,"Espuma de barbear",new BigDecimal(22.99));
+		Product product18 = new Product(null,"Vodka",new BigDecimal(28.90));
+		Product product19 = new Product(null,"Salsicha",new BigDecimal(4.00));
+		Product product20 = new Product(null,"Camarão",new BigDecimal(35.20));		
+		Product product21 = new Product(null,"Desodorante",new BigDecimal(11.50));
+		
+		productService.create(product1);
+		productService.create(product2);
+		productService.create(product3);
+		productService.create(product4);
+		productService.create(product5);
+		productService.create(product6);
+		productService.create(product7);
+		productService.create(product8);
+		productService.create(product9);
+		productService.create(product10);
+		productService.create(product11);
+		productService.create(product12);
+		productService.create(product13);
+		productService.create(product14);
+		productService.create(product15);
+		productService.create(product16);
+		productService.create(product17);
+		productService.create(product18);
+		productService.create(product19);
+		productService.create(product20);
+		productService.create(product21);
 	}
 
 	private void initialUsers() throws Throwable, BusinessException {
